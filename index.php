5803ef013d6eda74d8db204bfea5b784c51515dc
Pleiades7 <oracleoflight77@gmail.com>
2018-03-19 10:17:54 +0800

    Log Out

--- index.php
+++ index.php
@@ -10,18 +10,26 @@
 						<p>Impact a life, transform a community.</p>
 					</header>
 					<a href="#main" class="button scrolly">Learn More about the Jinjang Community</a>
-					<?php
-					if(isset($_SESSION['u_id'])){
-						echo "You are logged in";
-						echo '<form action="includes/logout.inc.php" method="POST">
-						<input type="submit" name="submit" class="btn" value="Log Out" /></input></form>';
-					}					 
-					?>
-					<p></p>
-					<ul class="actions">
-						<li><a href="register.php" class="button special">Register</a></li>
-						<li><a href="login.php" class="button alt">Login</a></li>
-					</ul>
+						<p></p>						
+						<?php
+							session_start();
+							if(isset($_SESSION['u_id']) == true){
+								?>
+							<div id="logintrue">
+								<h5>You are logged in. Welcome back, <?php echo $_SESSION['u_username']?></h5>
+								<form action="includes/logout.inc.php" method="POST">
+								<input type="submit" name="submit" class="btn" value="Log Out" /></input></form>	
+							</div>
+							<?php } else {?>
+							<div id="loginfalse">
+								<ul class="actions">
+									<li><a href="register.php" class="button special">Register</a></li>
+									<li><a href="login.php" class="button alt">Login</a></li>
+								</ul>
+							</div>
+							<?php
+							}
+						?>
 				</div>
 			</section>
 <?php
